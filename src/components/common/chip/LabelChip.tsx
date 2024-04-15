4dcb0b5dd9160f1275004f4a16518658b006c9e7
HaeJungg <gpwnd0120@gmail.com>
2024-04-15 23:21:13 +0900

    feat: 공통라벨 컴포넌트 구현(#7)

---
+++ src/components/common/chip/LabelChip.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './labelChip.module.scss';
+
+// 백그라운드와 텍스트 색상 배열
+const BACKGROUNDCOLORS = ['#F7CAC9', '#D3E0F0', '#FFD3B3', '#E0F2F6', '#F7E4F2', '#D7D7D7'];
+const TEXTCOLORS = ['#7ac555', '#760dde', '#ffa500', '#76a5ea', '#e876ea', '#4b4b4b'];
+
+// 색상 인덱스 카운터
+let colorIndex = 0;
+
+interface LabelChipProps {
+  size: 'large' | 'small';
+  label: string;
+  type: 'columns' | 'tag';
+}
+
+export default function LabelChip({ size, label, type }: LabelChipProps) {
+  // 컴포넌트가 생성될 때마다 색상 인덱스 업데이트
+  const backgroundColor = BACKGROUNDCOLORS[colorIndex % BACKGROUNDCOLORS.length];
+  const textColor = TEXTCOLORS[colorIndex % TEXTCOLORS.length];
+  colorIndex += 1;
+
+  const classNames = `${styles.labelChip} ${styles[size]} ${styles[type]}`;
+
+  return (
+    <div className={classNames} style={{ backgroundColor, color: textColor }}>
+      {type === 'columns' && <div className={styles.iconCircle} style={{ backgroundColor: textColor }} />}
+      {label}
+    </div>
+  );
+}
