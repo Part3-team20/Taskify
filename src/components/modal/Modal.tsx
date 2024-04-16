eb422f5517bf1eaabad4d3655d0480d14e6a4500
gun7741 <gun7741@naver.com>
2024-04-16 15:04:43 +0900

    feat: Modal 컴포넌트 (#5)

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,42 @@
+import React, { ReactNode, useRef, useEffect } from 'react';
+import styles from './Modal.module.scss';
+
+interface ModalProps {
+  children: ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  // eslint-disable-next-line react/require-default-props
+  style?: React.CSSProperties;
+}
+
+export default function Modal({ isOpen = false, children, onClose, style }: ModalProps) {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
+  if (!isOpen) return null;
+
+  return (
+    <div className={styles.container}>
+      <div ref={modalRef} className={styles.modal} style={style}>
+        {children}
+      </div>
+    </div>
+  );
+}
