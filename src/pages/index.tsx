fbd6067aaaed0d8abfb03e95663faf71e8b20f0f
HaeJungg <gpwnd0120@gmail.com>
2024-04-16 22:53:08 +0900

    feat: 플러스 칩 컴포넌트 추가(#7)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // eslint-disable-next-line import/extensions
 import LabelChip from '@/components/common/chip/LabelChip';
+// eslint-disable-next-line import/extensions
+import PlusChip from '@/components/common/chip/PlusChip';
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <LabelChip size="large" label="On Progress" type="columns" />
       <LabelChip size="small" label="프로젝트" type="tag" />
       <LabelChip size="large" label="일반" type="tag" />
+      <PlusChip type="small" />
+      <PlusChip type="large" />
     </div>
   );
 }
