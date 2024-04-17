85de6fe042c193345d867956ee20b1835bb483b8
gun7741 <gun7741@naver.com>
2024-04-17 20:31:56 +0900

    chore : 오타 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import '@/styles/globals.scss';
 
 export const metadata = {
-  title: 'Tasify',
-  description: 'Tasify 할일 관리',
+  title: 'Taskify',
+  description: 'Taskify 할일 관리',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
