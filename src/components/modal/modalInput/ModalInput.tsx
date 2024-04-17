cce006dc9639de6ee35600c26132a32a121ca6e1
gun7741 <gun7741@naver.com>
2024-04-17 22:47:42 +0900

    feat: modalInput style props 추가

--- src/components/modal/modalInput/ModalInput.tsx
+++ src/components/modal/modalInput/ModalInput.tsx
@@ -7,12 +7,14 @@ interface ModalInputProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   // eslint-disable-next-line react/require-default-props
   children?: ReactNode;
+  // eslint-disable-next-line react/require-default-props
+  style?: React.CSSProperties;
 }
 
-export default function ModalInput({ placeholder, value, onChange, children }: ModalInputProps) {
+export default function ModalInput({ placeholder, value, onChange, children, style }: ModalInputProps) {
   return (
     <div className={styles.container}>
-      <input className={styles.modalInput} placeholder={placeholder} value={value} onChange={onChange} />
+      <input className={styles.modalInput} placeholder={placeholder} value={value} onChange={onChange} style={style} />
       <div className={styles.childrenContainer}>{children}</div>
     </div>
   );
