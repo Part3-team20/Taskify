fbfb4db5a38dfdadbd790b8f074b326721c1f180
aoooec <foottable@gmail.com>
2024-04-17 22:54:57 +0900

    feat: 애드 버튼 추가(#6)

---
+++ src/components/button/AddButton/index.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+import Image from 'next/image';
+import classNames from 'classnames/bind';
+import styles from './AddButton.module.scss';
+
+const cx = classNames.bind(styles);
+
+interface AddButtonProps {
+  children?: ReactNode;
+  dashboard?: boolean;
+  handleClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export default function AddButton({ children, dashboard, handleClick }: AddButtonProps) {
+  return (
+    <button
+      className={cx('container', children ? 'hasChildren' : null, dashboard && 'dashboard')}
+      type="button"
+      onClick={handleClick}
+    >
+      {children}
+      <div className={styles.iconBox}>
+        <Image src="/images/add_fill_box.svg" fill alt="추가하기 아이콘" />
+      </div>
+    </button>
+  );
+}
+
+AddButton.defaultProps = {
+  children: null,
+  dashboard: false,
+};
