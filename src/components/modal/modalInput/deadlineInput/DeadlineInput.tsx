cbb4337454b065112bbf86baa39da72d293ded9c
gun7741 <gun7741@naver.com>
2024-04-17 19:55:16 +0900

    feat: 마감일 input (#12)

---
+++ src/components/modal/modalInput/deadlineInput/DeadlineInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import Image from 'next/image';
+import styles from './DeadlineInput.module.scss';
+import 'react-datepicker/dist/react-datepicker.css';
+
+export default function DeadLineInput() {
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.deadline}>마감일</p>
+      <div className={styles.inputContainer}>
+        <Image
+          className={styles.calendarIcon}
+          src="/images/calendar_icon.svg"
+          alt="calendarIcon"
+          width={20}
+          height={20}
+        />
+        <ReactDatePicker
+          className={styles.datePicker}
+          placeholderText="날짜를 입력해주세요."
+          closeOnScroll
+          selected={selectedDate}
+          onChange={handleDateChange}
+          showTimeSelect
+          timeFormat="HH:mm"
+          timeIntervals={15}
+          timeCaption="time"
+          dateFormat="yyyy.MM.dd h:mm aa"
+        />
+      </div>
+    </div>
+  );
+}
