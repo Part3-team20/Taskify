449b18b9967f87e2bef65ba4a9ab81fe84935b3c
gun7741 <gun7741@naver.com>
2024-04-17 19:56:05 +0900

    test: test용 코드

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import ExampleModal from '@/components/modal/ExampleModal';
+import ExampleModal from '@/components/modal/modalExample/ExampleModal';
 import Profile from '@/components/profile/Profile';
+
+import TitleInput from '@/components/modal/modalInput/titleInput/TitleInput';
+import DeadLineInput from '@/components/modal/modalInput/deadlineInput/DeadlineInput';
+import CommentInput from '@/components/modal/modalInput/commentInput/CommentInput';
 
 export default function Home() {
   return (
     <div>
       <ExampleModal />
       <Profile />
+      <TitleInput />
+      <DeadLineInput />
+      <CommentInput />
     </div>
   );
 }
