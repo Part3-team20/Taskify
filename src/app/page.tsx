d75d8423a8e4e27fe3e60e1bacd3d18cbc202488
gun7741 <gun7741@naver.com>
2024-04-18 01:17:16 +0900

    test : test코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-import ExampleModal from '@/components/modal/modalExample/ExampleModal';
-import CommentInput from '@/components/modal/modalInput/commentInput/CommentInput';
-import DeadLineInput from '@/components/modal/modalInput/deadlineInput/DeadlineInput';
-import TitleInput from '@/components/modal/modalInput/titleInput/TitleInput';
-import Profile from '@/components/profile/Profile';
 import React from 'react';
 
 export default function Home() {
-  return (
-    <div>
-      <ExampleModal />
-      <Profile />
-      <TitleInput />
-      <DeadLineInput />
-      <CommentInput />
-    </div>
-  );
+  return <div>home</div>;
 }
