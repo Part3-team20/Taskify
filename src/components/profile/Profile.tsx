6473080a4710914cdc147c240029e8c9491d66df
HaeJungg <gpwnd0120@gmail.com>
2024-04-19 00:03:05 +0900

    fix: 프로필 컴포넌트 수정(#26)

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,17 +1,22 @@
-'use client';
-
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Profile.module.scss';
 
 const BASE_PROFILE_IMG = '/images/profile.svg';
 
-export default function Profile() {
-  const [profileImg] = useState(BASE_PROFILE_IMG);
+interface ProfileProps {
+  // eslint-disable-next-line react/require-default-props
+  profileImageUrl?: string;
+}
 
+export default function Profile({ profileImageUrl }: ProfileProps) {
+  const imgSrc = profileImageUrl || BASE_PROFILE_IMG;
   return (
     <div className={styles.profile}>
-      <Image src={profileImg} alt="profileImg" width={23} height={23} />
+      <Image src={imgSrc} alt="profileImg" width={23} height={23} />
+      {/* 아직 이미지 호스트 주소를 알 수 없어서 이미지 확인을 위해 img 태그를 남겼습니다. */}
+      {/* 목데이터 활용할땐 아래 코드로 사용하시면 됩니다 */}
+      {/* <img src={imgSrc} alt="profileImg" width={23} height={23} /> */}
     </div>
   );
 }
