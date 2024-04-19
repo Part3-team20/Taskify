f4a846aa1bb85c8299c92dcb8f49efad9cafbd0e
Jin-Chanyong <cksdyd324@gmail.com>
2024-04-19 20:44:07 +0900

    feat: 랜딩페이지 header 구현(#9)

---
+++ src/components/common/Header/LandingPageHeader/index.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './LandingPageHeader.module.scss';
+
+export default function LandingPageHeader() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <Link href={'/'} className={styles.logo}>
+          <Image src="/images/mainLogo.svg" alt="logo" className={styles.logoImg} width={28.8} height={33.1} />
+          <Image src="/images/Taskify.svg" alt="taskify" className={styles.taskify} width={80} height={22} />
+        </Link>
+        <div className={styles.auth}>
+          <Link href={'/login'}>로그인</Link>
+          <Link href={'/signup'}>회원가입</Link>
+        </div>
+      </div>
+    </header>
+  );
+}
