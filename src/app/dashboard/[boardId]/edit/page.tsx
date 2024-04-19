1cbb188482048adb8fc00f8cd7129ab6888b8ead
aoooec <foottable@gmail.com>
2024-04-19 16:27:07 +0900

    design: 공통 레이아웃 페이지 적용(#31)

--- src/app/dashboard/[boardId]/edit/page.tsx
+++ src/app/dashboard/[boardId]/edit/page.tsx
@@ -1,5 +1,10 @@
+import CommonLayout from '@/layouts/CommonLayout';
 import styles from './BoardEdit.module.scss';
 
 export default function BoardEdit() {
-  return <div className={styles.container}>BoardEdit 페이지</div>;
+  return (
+    <CommonLayout>
+      <div className={styles.container}>BoardEdit 페이지</div>
+    </CommonLayout>
+  );
 }
