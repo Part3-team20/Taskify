541d55d1fe92fc92e9fa53733f81dadd0d3db776
Chaemin-153 <0520032@gmail.com>
2024-04-22 01:11:17 +0900

    feat: 회원가입시 비밀번호 8자 이상 입력 조건 추가(#8)

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -14,8 +14,10 @@ export default function SignUpPage() {
     email: '',
     nickname: '',
     password: '',
+    confirmPassword: '',
   });
   const [isBtnActive, setIsBtnActive] = useState(false);
+  const [isMoreThanEight, setIsMoreThanEight] = useState(true);
 
   function handleChange(e: any) {
     const { name, value } = e.target;
@@ -43,10 +45,23 @@ export default function SignUpPage() {
   };
 
   useEffect(() => {
-    const { email, nickname, password } = values;
+    const { email, nickname, password, confirmPassword } = values;
+
+    // 입력 폼 모두 입력시, 버튼 활성화
     const isValid =
-      email.trim() !== '' && nickname.trim() !== '' && password.trim() !== '' && password.trim().length >= 8;
+      email.trim() !== '' &&
+      nickname.trim() !== '' &&
+      password.trim() !== '' &&
+      password.trim().length >= 8 &&
+      password === confirmPassword;
     setIsBtnActive(isValid);
+
+    // 비밀번호 8자 이하 확인
+    if (password.trim().length > 0 && password.trim().length < 8) {
+      setIsMoreThanEight(false);
+    } else {
+      setIsMoreThanEight(true);
+    }
   }, [values]);
 
   return (
@@ -68,7 +83,6 @@ export default function SignUpPage() {
             placeholder="이메일을 입력해 주세요"
           />
         </label>
-        <br />
         <label htmlFor="user_nickname" className={styles.label}>
           닉네임
           <input
@@ -81,7 +95,6 @@ export default function SignUpPage() {
             placeholder="닉네임을 입력해 주세요"
           />
         </label>
-        <br />
         <label htmlFor="user_pw" className={styles.label}>
           비밀번호
           <input
@@ -94,10 +107,27 @@ export default function SignUpPage() {
             onChange={handleChange}
           />
         </label>
-        <br />
-        {/* <input type="submit" value="가입하기" /> */}
+        {!isMoreThanEight && <p className={styles.errorMessage}>8자 이상 입력해주세요.</p>}
+        <label htmlFor="user_confirmPw" className={styles.label}>
+          비밀번호 확인
+          <input
+            className={styles.input}
+            id="user_confirmPw"
+            type="password"
+            name="confirmPassword"
+            placeholder="비밀번호를 한번 더 입력해 주세요"
+            required
+            onChange={handleChange}
+          />
+        </label>
         <LoginSubmitButton isActive={isBtnActive}>가입하기</LoginSubmitButton>
       </form>
+      <p className={styles.signupText}>
+        이미 가입하셨나요?
+        <Link href="/login">
+          <span>로그인하기</span>
+        </Link>
+      </p>
     </div>
   );
 }
