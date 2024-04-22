294b7e0acc1d038f7877ac0fdbffeae01b15e025
gun7741 <gun7741@naver.com>
2024-04-23 00:13:38 +0900

    feat : 컴포넌트 페이지에 구현 (#30)

--- src/app/dashboard/[boardId]/edit/page.tsx
+++ src/app/dashboard/[boardId]/edit/page.tsx
@@ -1,10 +1,37 @@
+'use client';
+
+import { useParams } from 'next/navigation';
 import CommonLayout from '@/layouts/CommonLayout';
+import DeleteDashboardButton from '@/components/common/Button/DeleteDashboardButton';
+import DashboaradChange from './editComponents/DashboardChange';
+import PreviosPageButton from './editComponents/PreviousPageButton';
+import MemberManagement from './editComponents/MemberManagement';
+import InviteStatus from './editComponents/InviteStatus';
 import styles from './BoardEdit.module.scss';
 
 export default function BoardEdit() {
+  /* 현재 페이지의 id를 받아와 각 컴포넌트에 id값 전달
+      id는 CommonLayout에서 받아와야함.
+  */
+
+  const { boardId } = useParams();
+
+  console.log(boardId); // 타입 string
+
+  const handleDeleteDashboard = (id: any) => {
+    /* TODO 대쉬보드 삭제 구현 */
+    console.log(id, '삭제');
+  };
+
   return (
     <CommonLayout>
-      <div className={styles.container}>BoardEdit 페이지</div>
+      <div className={styles.container}>
+        <PreviosPageButton />
+        <DashboaradChange id={boardId} />
+        <MemberManagement id={boardId} />
+        <InviteStatus id={boardId} />
+        <DeleteDashboardButton handleDelete={() => handleDeleteDashboard(boardId)} />
+      </div>
     </CommonLayout>
   );
 }
