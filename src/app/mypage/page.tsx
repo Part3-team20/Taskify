4141407e51db35ca919a79ff71b1db5e6367ea9f
Jin-Chanyong <cksdyd324@gmail.com>
2024-04-22 15:50:15 +0900

    feat: 뒤로가기 버튼 구현(#34)

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,10 +1,19 @@
 import CommonLayout from '@/layouts/CommonLayout';
 import styles from './MyPage.module.scss';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function MyPage() {
+  const router = useRouter();
+
   return (
     <CommonLayout>
-      <div className={styles.container}>MyPage 페이지</div>
+      <div className={styles.container}>
+        <button className={styles.goBackButton} onClick={() => router.back()}>
+          <Image src={'/images/arrow_icon.svg'} alt="돌아가기" width={20} height={20} style={{ rotate: '180deg' }} />
+          돌아가기
+        </button>
+      </div>
     </CommonLayout>
   );
 }
