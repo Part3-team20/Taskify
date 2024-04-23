ea15821823ad251006f2daef54f2d8bae5ed7c96
HaeJungg <gpwnd0120@gmail.com>
2024-04-23 22:15:46 +0900

    feat: design: 컬럼 컴포넌트 스타일링

--- src/components/Column/index.tsx
+++ src/components/Column/index.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from 'react';
 import Image from 'next/image';
 import styles from './Column.module.scss';
@@ -5,9 +7,17 @@ import Card from '../Card/Card';
 import NumberChip from '../common/Chip/NumberChip';
 import AddButton from '../common/Button/AddButton';
 
+interface CardProps {
+  title: string;
+  tags?: string[];
+  imageUrl?: string;
+  profileImageUrl?: string;
+  dueDate?: string;
+}
+
 interface ColumnProps {
   title: string;
-  cards: [{ title: string; tags?: string[]; profileImageUrl?: string; imageUrl?: string; dueDate?: string }];
+  cards: CardProps[];
   onAddCard: () => void;
 }
 
@@ -16,14 +26,18 @@ export default function Column({ title, cards, onAddCard }: ColumnProps) {
     <div className={styles.column}>
       <div className={styles.columnHeader}>
         <div className={styles.columnTitle}>
-          <div className={styles.iconCircle} />
-          <h2>{title}</h2>
+          <div className={styles.title}>
+            <div className={styles.iconCircle} />
+            <h2>{title}</h2>
+          </div>
           <NumberChip number={cards.length} />
         </div>
-        <Image src="/images/settings_icon.svg" fill alt="설정 아이콘" />
+        <Image className={styles.setting} src="/images/settings_icon.svg" width={24} height={24} alt="설정 아이콘" />
       </div>
       <div className={styles.columnBody}>
-        <AddButton handleClick={onAddCard} />
+        <div className={styles.addBtn}>
+          <AddButton handleClick={onAddCard} />
+        </div>
         {cards.map((card) => (
           <Card
             title={card.title}
