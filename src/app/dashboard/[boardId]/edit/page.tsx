9e6073420f10a0ebfd1a04335355c13fa34be04f
gun7741 <gun7741@naver.com>
2024-04-24 23:27:49 +0900

    feat : 대쉬보드 삭제 구현 (#30)

--- src/app/dashboard/[boardId]/edit/page.tsx
+++ src/app/dashboard/[boardId]/edit/page.tsx
@@ -1,36 +1,43 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import CommonLayout from '@/layouts/CommonLayout';
 import DeleteDashboardButton from '@/components/common/Button/DeleteDashboardButton';
-import DashboaradChange from './editComponents/DashboardChange';
 import PreviosPageButton from './editComponents/PreviousPageButton';
 import MemberManagement from './editComponents/MemberManagement';
 import InviteStatus from './editComponents/InviteStatus';
 import styles from './BoardEdit.module.scss';
+import DashboaradChange from './editComponents/DashboardChange';
+import useFetchWithToken from '@/hooks/useFetchToken';
 
 export default function BoardEdit() {
-  /* 현재 페이지의 id를 받아와 각 컴포넌트에 id값 전달
+  /* 현재 페이지의 boardId를 받아와 각 컴포넌트에 id값 전달
       id는 CommonLayout에서 받아와야함.
   */
-
+  const { fetchWithToken } = useFetchWithToken();
+  const router = useRouter();
   const { boardId } = useParams();
 
-  console.log(boardId); // 타입 string
+  const id = Number(boardId);
 
-  const handleDeleteDashboard = (id: any) => {
-    /* TODO 대쉬보드 삭제 구현 */
-    console.log(id, '삭제');
+  const handleDeleteDashboard = async (dashboardId: number) => {
+    /* 대쉬보드 삭제  */
+    try {
+      await fetchWithToken(`https://sp-taskify-api.vercel.app/4-20/dashboards/${dashboardId}`, 'DELETE');
+      router.push('/mydashboard');
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
     <CommonLayout>
       <div className={styles.container}>
         <PreviosPageButton />
-        <DashboaradChange id={boardId} />
-        <MemberManagement id={boardId} />
-        <InviteStatus id={boardId} />
-        <DeleteDashboardButton handleDelete={() => handleDeleteDashboard(boardId)} />
+        <DashboaradChange boardId={id} />
+        <MemberManagement boardId={id} />
+        <InviteStatus boardId={id} />
+        <DeleteDashboardButton handleDelete={() => handleDeleteDashboard(id)} />
       </div>
     </CommonLayout>
   );
