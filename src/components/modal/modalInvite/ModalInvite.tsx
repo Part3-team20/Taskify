1d2cb2e278c2a7b4b7e0619c4ff4d92b50ab7b1d
gun7741 <gun7741@naver.com>
2024-04-24 23:30:46 +0900

    feat : 대시보드 초대하기 로직 구현 (#30)

--- src/components/modal/modalInvite/ModalInvite.tsx
+++ src/components/modal/modalInvite/ModalInvite.tsx
@@ -1,21 +1,36 @@
 'use client';
 
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
+import useFetchWithToken from '@/hooks/useFetchToken';
 import Button from '@/components/common/Button/Button';
 import Image from 'next/image';
 import Modal from '../Modal';
-
 import styles from './ModalInvite.module.scss';
 
-export default function ModalInvite() {
+export default function ModalInvite({ boardId }: { boardId: number }) {
   const [isOpen, setIsOpen] = useState(false);
+  const [emailValue, setEmailValue] = useState('');
+
+  const { fetchWithToken } = useFetchWithToken();
 
   const handleClickCancel = () => {
     setIsOpen(false);
   };
 
-  const handlePostInvite = () => {
+  const handleChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmailValue(e.target.value);
+  };
+
+  const handlePostInvite = async () => {
     /* TODO : 초대하기  로직 */
+    try {
+      await fetchWithToken(`https://sp-taskify-api.vercel.app/4-20/dashboards/${boardId}/invitations`, 'POST', {
+        email: emailValue,
+      });
+      setIsOpen(false);
+    } catch (e) {
+      console.error(e);
+    }
   };
 
   return (
@@ -28,7 +43,12 @@ export default function ModalInvite() {
         <div className={styles.modalContainer}>
           <p className={styles.invite}>초대하기</p>
           <p className={styles.email}>이메일</p>
-          <input className={styles.emailInput} placeholder="이메일 입력" />
+          <input
+            className={styles.emailInput}
+            placeholder="이메일 입력"
+            value={emailValue}
+            onChange={handleChangeEmail}
+          />
           <div className={styles.btnContainer}>
             <Button color="white" cancel handleClick={handleClickCancel}>
               취소
