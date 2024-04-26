8700a3e0420aa701e607ceace9b5462757a0981f
aoooec <foottable@gmail.com>
2024-04-26 21:26:44 +0900

    add: error, loading, notFound 페이지 생성 및 스타일링(#69)

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Button from '@/components/common/Button/Button';
 import styles from './defaultPageStyle.module.scss';
 
-export default function NotFound() {
+export default function ErrorPage() {
   const returnPage = () => {
     window.history.back();
   };
@@ -15,8 +15,8 @@ export default function NotFound() {
       <Link href="/">
         <Image src="/images/mainLogo.svg" width={100} height={100} alt="taskify" priority />
       </Link>
-      <h1 className={styles.subtitle}>Page Not Found 😵‍💫</h1>
-      <p className={styles.desc}>존재하지 않는 페이지예요.</p>
+      <h1 className={styles.title}>Error! 😵‍💫</h1>
+      <p className={styles.desc}>페이지를 불러오는데 실패했어요.</p>
       <Button color="violet" handleClick={returnPage}>
         돌아가기
       </Button>
