6d450f6551cff702a827d308300fcbc3316cd5ae
aoooec <foottable@gmail.com>
2024-04-26 17:01:58 +0900

    fix: 일부 충돌 코드 수정

--- src/components/common/Header/LandingPageHeader/index.tsx
+++ src/components/common/Header/LandingPageHeader/index.tsx
@@ -6,13 +6,13 @@ export default function LandingPageHeader() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <Link href={'/'} className={styles.logo}>
+        <Link href="/" className={styles.logo}>
           <Image src="/images/mainLogo.svg" alt="logo" className={styles.logoImg} width={28.8} height={33.1} />
           <Image src="/images/Taskify.svg" alt="taskify" className={styles.taskify} width={80} height={22} />
         </Link>
         <div className={styles.auth}>
-          <Link href={'/login'}>로그인</Link>
-          <Link href={'/signup'}>회원가입</Link>
+          <Link href="/login">로그인</Link>
+          <Link href="/signup">회원가입</Link>
         </div>
       </div>
     </header>
