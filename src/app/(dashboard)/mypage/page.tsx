63f00a8b2e71c5d0d7e23b419193189dd08a5d7c
aoooec <foottable@gmail.com>
2024-04-27 15:37:40 +0900

    fix: 새로 작성된 부분으로 코드 변경

--- src/app/(dashboard)/mypage/page.tsx
+++ src/app/(dashboard)/mypage/page.tsx
@@ -1,74 +1,133 @@
 'use client';
 
-import { ChangeEvent, FormEvent, MouseEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useCallback, useEffect, useState } from 'react';
 import GoBackButton from '@/components/common/Button/GoBackButton';
 import FileInput from '@/components/common/FileInput';
 import Input from '@/components/common/Input';
 import BasicSubmitButton from '@/components/common/Button/BasicSubmitButton';
+import useFetchWithToken from '@/hooks/useFetchToken';
+import { CHANGE_PASSWORD, USERS } from '@/constants/ApiUrl';
+import { useRouter } from 'next/navigation';
+import Toast from '@/util/Toast';
+
 import styles from './MyPage.module.scss';
 
-const mockData = {
-  id: 1,
-  email: 'cksdyd324@gmail.com',
-  nickname: '진찬용',
-  profileImageUrl: undefined,
-  createdAt: '2024-04-19T12:30:21.029Z',
-  updatedAt: '2024-04-19T12:30:21.029Z',
-};
-
 export default function MyPage() {
+  const router = useRouter();
+  const { fetchWithToken } = useFetchWithToken();
   const [profile, setProfile] = useState<{ nickName: string; profileImageUrl: string | undefined }>({
-    nickName: mockData.nickname,
-    profileImageUrl: mockData.profileImageUrl,
+    nickName: '',
+    profileImageUrl: '',
   });
   const [imageFile, setImageFile] = useState<File | null>(null);
-  const [password, setPassword] = useState({ password: '', newPassword: '', passwordCheck: '' });
+  const [passwords, setPasswords] = useState({ password: '', newPassword: '', passwordCheck: '' });
+  const [email, setEmail] = useState<string | undefined>('');
+
+  const fetchUserData = useCallback(async () => {
+    try {
+      const result = await fetchWithToken(`${USERS}`, 'GET');
+      setProfile({
+        nickName: result.nickname,
+        profileImageUrl: result.profileImageUrl,
+      });
+      setEmail(result.email);
+    } catch (error) {
+      console.error('Failed to fetch comments:', error);
+    }
+  }, []);
 
   const handleNickNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setProfile((prev) => ({ ...prev, nickName: e.target.value }));
   };
 
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  const handlePasswordChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswords((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
-  const handleProfileSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleProfileSubmit = async (e: MouseEvent<HTMLButtonElement>) => {
+    try {
+      const formData = new FormData();
+      const profileBody = {
+        nickname: profile.nickName,
+        profileImageUrl: undefined,
+      };
+      const ImageBody = formData;
+      const accessToken = localStorage.getItem('accessToken');
+      await fetchWithToken(`https://sp-taskify-api.vercel.app/4-20/users/me`, 'PUT', profileBody);
+      await fetch(`https://sp-taskify-api.vercel.app/4-20/users/me/image`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: ImageBody,
+      });
+    } catch (error) {
+      console.error('Failed to update profile:', error);
+    }
   };
 
-  const handlePasswordSubmit = (e: FormEvent<HTMLFormElement>) => {
+  const handlePasswordSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    const { password, newPassword } = passwords;
+
+    try {
+      await fetchWithToken(`${CHANGE_PASSWORD}`, 'PUT', {
+        password,
+        newPassword,
+      });
+      Toast.success('비밀번호를 변경했습니다');
+      router.refresh();
+    } catch (err: any) {
+      // Error 객체에서 Message만 추출
+      const errorMessage = err.toString().substr(7);
+      Toast.error(errorMessage);
+    }
   };
 
   const handleLogoutClick = () => {
     window.localStorage.removeItem('accessToken');
   };
 
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
   return (
     <div className={styles.container}>
       <GoBackButton />
-      <form onSubmit={handleProfileSubmit} className={styles.profileForm}>
+      <form onSubmit={(e) => e.preventDefault()} className={styles.profileForm}>
         <h1>프로필</h1>
         <div className={styles.inputs}>
-          <FileInput setFile={setImageFile} className={styles.fileInput} />
+          <FileInput
+            setFile={setImageFile}
+            className={styles.fileInput}
+            defaultImage={profile?.profileImageUrl || ''}
+          />
           <div className={styles.textInputs}>
-            <Input labelName="이메일" name="email" placeholder={mockData.email} disabled />
+            <Input labelName="이메일" name="email" placeholder={email} disabled />
             <Input
               labelName="닉네임"
               name="nickName"
-              value={profile.nickName}
+              value={profile?.nickName}
               required
               onChange={handleNickNameChange}
             />
           </div>
         </div>
         <div className={styles.button}>
-          <BasicSubmitButton color="violet" isActive={Boolean(profile.nickName)} handleClick={() => {}}>
+          <BasicSubmitButton
+            color="violet"
+            isActive={Boolean(profile?.nickName)}
+            handleClick={handleProfileSubmit}
+            type="button"
+          >
             저장
           </BasicSubmitButton>
         </div>
       </form>
-      <form onSubmit={handlePasswordSubmit} className={styles.passwordForm}>
+      <form onSubmit={(e) => e.preventDefault()} className={styles.passwordForm}>
         <h1>비밀번호 변경</h1>
         <div className={styles.inputs}>
           <Input
@@ -76,6 +135,7 @@ export default function MyPage() {
             name="password"
             placeholder="현재 비밀번호 입력"
             onChange={handlePasswordChange}
+            type="password"
             required
           />
           <Input
@@ -83,6 +143,7 @@ export default function MyPage() {
             name="newPassword"
             placeholder="새 비밀번호 입력"
             onChange={handlePasswordChange}
+            type="password"
             required
           />
           <Input
@@ -90,11 +151,19 @@ export default function MyPage() {
             name="passwordCheck"
             placeholder="새 비밀번호 입력"
             onChange={handlePasswordChange}
+            type="password"
             required
           />
         </div>
         <div className={styles.button}>
-          <BasicSubmitButton color="violet" isActive handleClick={() => {}}>
+          <BasicSubmitButton
+            color="violet"
+            isActive={Boolean(
+              password?.password && password?.newPassword && password?.newPassword === password.passwordCheck
+            )}
+            handleClick={handlePasswordSubmit}
+            type="button"
+          >
             변경
           </BasicSubmitButton>
         </div>
